62f39487b034a06da6e9a1eb6ccf94955fe6ff3f
Tim Banks <thetimbanks@gmail.com>
2020-02-19 09:35:48 -0600

    Access environment variables using process.env

--- handler.js
+++ handler.js
@@ -4,24 +4,24 @@ const sgMail = require('@sendgrid/mail');
 const geolib = require('geolib');
 
 module.exports.reminder = (event, context, callback) => {
-  sgMail.setApiKey(SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const options = {
-    authToken: TESLA_API_AUTH_TOKEN,
-    vehicleID: TESLA_VEHICLE_ID,
+    authToken: process.env.TESLA_API_AUTH_TOKEN,
+    vehicleID: process.env.TESLA_VEHICLE_ID,
   };
 
   teslaApi.vehicleDataAsync(options).done(function(vehicleData) {
       const chargeState = vehicleData.charge_state;
       const driveState = vehicleData.drive_state;
       const carLocation = [driveState.latitude, driveState.longitude];
-      const homeLocation = [HOME_LATITUDE, HOME_LONGITUDE];
+      const homeLocation = [process.env.HOME_LATITUDE, process.env.HOME_LONGITUDE];
       const distance = geolib.getDistance(carLocation, homeLocation);
       const shouldSendTextMessage = distance < 100 && (chargeState.charging_state !== "Charging" || chargeState.charging_state !== "Complete");
 
       if (shouldSendTextMessage) {
         const msg = {
-          to: EMAIL_TO,
-          from: EMAIL_FROM,
+          to: process.env.EMAIL_TO,
+          from: process.env.EMAIL_FROM,
           subject: 'Charging Reminder',
           text: 'Plug in your car!',
           html: '<strong>Plug in your car</strong>',
